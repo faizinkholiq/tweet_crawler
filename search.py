9c228c13691956b4a8bae94df2792511632cf84e
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-11-22 18:52:42 +0700

    refactor code

--- search.py
+++ search.py
@@ -11,7 +11,6 @@ consumer_key = os.environ.get('CONSUMER_KEY')
 consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
-client = tweepy.Client(bearer_token=os.environ.get('BEARER_TOKEN'))
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
